e8bd769760ef1794b071ea9e427d80aba1df7941
kana <kana-github@qnet.li>
2025-08-12 11:59:08 +0400

    update pocket-sync

--- Casks/pocket-sync.rb
+++ Casks/pocket-sync.rb
@@ -2,8 +2,8 @@ cask "pocket-sync" do
   name "Pocket Sync"
   desc "GUI tool for doing stuff with the Analogue Pocket"
   homepage "https://github.com/neil-morrison44/pocket-sync"
-  
-  version "5.5.0"
+
+  version "5.7.5"
   url "https://github.com/neil-morrison44/pocket-sync/releases/download/v#{version}/Pocket.Sync_#{version}_universal.dmg"
   sha256 "452af252f1f1aaf18e10cf5e438f99f9b7ae2e28da05b985e58039dd3282deb9"
 
